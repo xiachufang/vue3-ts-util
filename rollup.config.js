d5ec78f84a0acd98e22012d422f496a37ee59392
zanllp <qc@zanllp.cn>
2021-07-22 16:46:57 +0800

    build: 删除多余依赖

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
-import scss from "rollup-plugin-scss";
 import ts from "rollup-plugin-typescript2";
 import vue from "rollup-plugin-vue";
-import postcss from 'rollup-plugin-postcss'
-import autoprefixer from 'autoprefixer'
 import autoExternal from "rollup-plugin-auto-external";
 import { getBabelInputPlugin } from '@rollup/plugin-babel'
 import styles from "rollup-plugin-styles";
 
 const getPlugins = (esm = true) => [
   vue({ preprocessStyles: true  }),
-  postcss([autoprefixer()]),
+  styles(),
   autoExternal(),
   getBabelInputPlugin({
     include: ['src/**/*.tsx'],
