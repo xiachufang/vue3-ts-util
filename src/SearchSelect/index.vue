d0ec633bfc2768eaceffe4f0bc4564428f28862c
zanllp <qc@zanllp.cn>
2021-09-03 17:26:27 +0800

    refactor(SearchSelect): 减少必要的参数

--- src/SearchSelect/index.vue
+++ src/SearchSelect/index.vue
@@ -3,25 +3,25 @@
     v-model:value="selected"
     :get-popup-container="trigger => trigger.parentNode"
     placeholder="请选择"
-    :mode="mode"
     :filter-option="false"
     @search="onSearch"
     @blur="searchTarget = ''"
     :options="currOptions"
     optionLabelProp="title"
     show-search
+    v-bind="$attrs"
   />
 </template>
 
 <script lang="ts">
 import { customPropType } from '../'
 import { computed, defineComponent, ref } from 'vue'
-import { defaultConv, useOptionsComputed } from '.'
+import { useOptionsComputed } from '.'
+import { SearchSelectConv } from './typedef'
 
 export default defineComponent({
   props: {
     value: customPropType<any>(),
-    mode: customPropType((): 'multiple' | undefined => undefined),
     /**
      * 选项数组
      */
@@ -29,7 +29,7 @@ export default defineComponent({
     /**
      * 配置如何将选项转换成显示的文本，值，键
      */
-    conv: customPropType(() => defaultConv)
+    conv: customPropType<SearchSelectConv<any>>()
   },
   setup (props, ctx) {
     const searchTarget = ref('') // 当前搜索目标
